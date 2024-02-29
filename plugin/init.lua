a21f6b87565ee98abee5364487d5ede28d05a854
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-02-29 15:46:42 +0000

    investigating confusing selection / marks issue. no solution.

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,13 +1,20 @@
 -- this is will be run if you have installed the plugins
 -- you don't need to require("module")
-
-print("automatically run")
-
 forge_link = function()
-	-- todo copy to clipboard and print a message
-	print(require("forge-link").github_link())
+	-- one selection behind on the marks / line nums
+	-- example fist usage is alway 0,0 then the next usage is what the first should have been
+	-- something to do with the way I trigger this function in my keymaps / nvim config
+	--
+	-- works
+	-- keymap("v", "<leader>l", ":<C-u>ForgeLink<cr>", opts)
+	--
+	-- problem
+	-- keymap("v", "<leader>l", "<cmd>ForgeLink<cr>", opts)
+
+	local maybeLink = require("forge-link").github_link()
+	vim.fn.setreg("+", maybeLink)
+	print(maybeLink)
 end
 
 -- generate user commands or keybindings etc for the consumer of the plugins
--- getting no range allowed?
 vim.api.nvim_create_user_command("ForgeLink", forge_link, {})
