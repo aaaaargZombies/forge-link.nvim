f848928b144d6e5763ada4916fd3d33a697eed31
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-03-01 15:37:25 +0000

    minimal problemo of my visual_select_line_nums

--- plugin/init.lua
+++ plugin/init.lua
@@ -18,3 +18,19 @@ end
 
 -- generate user commands or keybindings etc for the consumer of the plugins
 vim.api.nvim_create_user_command("ForgeLink", forge_link, {})
+
+-- example visual selection marks issue for reddit help post
+-- https://www.reddit.com/r/neovim/comments/1b3uarv/trouble_getting_start_and_end_position_of_a/
+local function visual_select_line_nums()
+	local start_pos = vim.api.nvim_buf_get_mark(0, "<")
+	local end_pos = vim.api.nvim_buf_get_mark(0, ">")
+
+	local start_line = start_pos[1]
+	local end_line = end_pos[1]
+
+	print(start_line, end_line)
+end
+
+vim.api.nvim_create_user_command("VLines", visual_select_line_nums, {})
+
+vim.api.nvim_set_keymap("v", "<leader>vl", "<cmd>VLines<cr>", { noremap = true })
