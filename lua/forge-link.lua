a57596265ab8358c6788e49244c321d8076b4248
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-03-04 21:39:19 +0000

    add more notes

--- lua/forge-link.lua
+++ lua/forge-link.lua
@@ -67,7 +67,7 @@ end
 
 -- TODO : see if I can check for different remotes and turn this into forge_link instead of just github
 --
--- bitbucket
+--  bitbucket
 --  https://bitbucket.org/USER/REPO/annotate/COMMIT_HASH_or_BRANH/FILE_PATH_INCLUDING_NAME#FILE_NAME-3:7
 --  https://bitbucket.org/crumbletown/business/annotate/90959bf8a4bbc77d9c326d0a25cd27caebd39f71/re-innovation/elements.md#elements.md-3:7
 --
@@ -76,6 +76,11 @@ end
 --  https://codeberg.org/simonrepp/faircamp/src/commit/75f93a7313a2d9fe380f9e9822d8023f7e482dd6/src/util.rs#L5-L8
 --  https://codeberg.org/USER/REPO/src/commit/COMMIT_HASH/FILE_PATH#L5-L8
 --  https://codeberg.org/USER/REPO/src/branch/BRANCH/FILE_PATH#L5-L8
+--
+--  gitlab
+--  https://gitlab.com/spritely/goblins/blob/master/goblins/core.rkt?#L10-14
+--  https://gitlab.com/USER/REPO/blob/BRANCH/FILE_PATH?#L10-14
+--  https://gitlab.com/USER/REPO/blob/COMMIT_HASH/FILE_PATH?#L10-14
 
 ---@param remote string
 ---@param branch_name string
