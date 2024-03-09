4e0c5826615ee0636dabdd869ba7c05e190a5368
aaaaargZombies <6824891+aaaaargZombies@users.noreply.github.com>
2024-03-09 08:19:19 +0000

    add command to open link in browser

--- plugin/init.lua
+++ plugin/init.lua
@@ -6,5 +6,12 @@ local get_link = function()
 	print(maybeLink)
 end
 
+local nav_link = function()
+	local maybeLink = require("forge-link").forge_link()
+	os.execute("$BROWSER " .. maybeLink)
+	print("navigating to " .. maybeLink)
+end
+
 -- generate user commands or keybindings etc for the consumer of the plugins
 vim.api.nvim_create_user_command("ForgeLink", get_link, { range = true })
+vim.api.nvim_create_user_command("ForgeNav", nav_link, { range = true })
